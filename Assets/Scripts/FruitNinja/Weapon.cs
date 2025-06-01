dba875e4ce04af40eb1ed50de5526fd275204a83
NachoMoren <79315360+NachoMoren@users.noreply.github.com>
2025-06-01 16:11:04 +0200

    Added slice to fruit ninja

--- Assets/Scripts/FruitNinja/Weapon.cs
+++ Assets/Scripts/FruitNinja/Weapon.cs
@@ -1,11 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using EzySlice;
 
 public class Weapon : MonoBehaviour
 {
     public GameManager gameManager;
     public FruitUI UI;
+
+    public Transform startSlicePoint;
+    public Transform endSlicePoint; 
+    public LayerMask sliceableLayer;
+    public VelocityEstimator estimator; 
+
+    public Material crossSectionMaterial;
+    public float cutForce = 2000;
+
+
+
+    public void Update()
+    {
+        bool hasHit = Physics.Linecast(startSlicePoint.position, endSlicePoint.position, out RaycastHit hitInfo, sliceableLayer);
+        if (hasHit)
+        {
+            GameObject target = hitInfo.transform.gameObject;
+            Slice(target);
+        }
+    }
+
+    public void Slice(GameObject target)
+    {
+        Vector3 velocity = estimator.GetVelocityEstimate();
+        Vector3 planeNormal = Vector3.Cross(endSlicePoint.position - startSlicePoint.position, velocity).normalized;
+        planeNormal.Normalize();
+
+
+        SlicedHull hull = target.Slice(endSlicePoint.position, planeNormal);
+
+        if (hull != null)
+        {
+            GameObject upperHull = hull.CreateUpperHull(target, crossSectionMaterial);
+            SetupSlicedComponent(upperHull);
+            GameObject lowerHull = hull.CreateLowerHull(target, crossSectionMaterial);
+            SetupSlicedComponent(lowerHull);
+
+            Destroy(target);
+        }
+    }
+
+    public void SetupSlicedComponent(GameObject slicedObj)
+    {
+        Rigidbody rb = slicedObj.AddComponent<Rigidbody>();
+        MeshCollider collider = slicedObj.AddComponent<MeshCollider>();
+        collider.convex = true;
+        rb.AddExplosionForce(cutForce, slicedObj.transform.position, 1f);
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
